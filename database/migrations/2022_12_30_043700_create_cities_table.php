bf374ec26041c60cf3f52bcab9f8896867078d5e
Yudana Satria Kinanto <yudanasatria@gmail.com>
2022-12-30 14:08:36 +0700

    Add Database

--- database/migrations/2022_12_30_043700_create_cities_table.php
+++ database/migrations/2022_12_30_043700_create_cities_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('state_id');
+            $table->string('name');
             $table->timestamps();
         });
     }
