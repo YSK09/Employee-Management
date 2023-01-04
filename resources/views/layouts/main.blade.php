856fb893abd47b1b1ec9c6ce4a9c5d7fe0420460
Yudana Satria Kinanto <yudanasatria@gmail.com>
2023-01-04 10:26:15 +0700

    Add User Manajemen UI

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -34,7 +34,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="{{route('home')}}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -78,7 +78,7 @@
                 </a>
                 <div id="collapseUser" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="buttons.html">User</a>
+                        <a class="collapse-item" href="{{route('users.index')}}">User</a>
                         <a class="collapse-item" href="cards.html">Role</a>
                         <a class="collapse-item" href="cards.html">Permission</a>
                     </div>
@@ -146,12 +146,7 @@
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-                    </div>
-                    <div class="row">
                         @yield('content')
-                    </div>
                 </div>
                 <!-- /.container-fluid -->
 
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">User</h1>
+</div>
+<div class="row">
+    <div class="card mx-auto">
+    <div class="card-header">
+        Create New User
+    </div>
+    <div class="card-body">
+        FORM
+    </div>
+</div>
+</div>
+@endsection
